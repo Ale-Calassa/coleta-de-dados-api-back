7d0dd6557840c931cc1c87ad1d738c47259541be
Ale-Calaça <alesson.calassa@gmail.com>
2025-11-13 00:47:52 -0300

    Update

--- database.py
+++ database.py
@@ -8,6 +8,6 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL, connect_args={"sslmode": "require"})
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
