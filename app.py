e9280b5907c7b761500002b3a9ef41140649c362
Ale-Calaça <alesson.calassa@gmail.com>
2025-11-12 23:43:05 -0300

    Update File

--- app.py
+++ app.py
@@ -1,22 +1,23 @@
 from flask import Flask, request, jsonify
-from flask_cors import CORS
+from flask_cors import CORS  # 🔹 Importa o CORS
 from database import SessionLocal, engine, Base
 from models import DadosComerciais
 from datetime import datetime
 import os
 
+# Criando a aplicação do "Flask".
 app = Flask(__name__)
-CORS(app, origins=["https://ale-calassa.github.io"] supports_credentials=True)
+CORS(app, origins=["https://ale-calassa.github.io/Formulario-Coleta-de-Dados-Vendas/"])  # 🔓 Libera acesso apenas do frontend
 
+# Para Criar a tabela no DB caso não exista
 Base.metadata.create_all(bind=engine)
 
-@app.route('/')
-def index():
-    return jsonify({"mensagem": "API Coleta de Dados está rodando com sucesso!"})
-
+# Criando a Rota pra receber os dados via "POST".
 @app.route('/dados', methods=['POST'])
 def receber_dados():
-    data = request.get_json()
+    data = request.get_json() # Forma de Pegar os dados enviados no formato JSON
+
+    # Para calcular o valor total da venda
     valor_total = int(data["quantidade"]) * float(data["valor_unitario"])
     session = SessionLocal()
 
